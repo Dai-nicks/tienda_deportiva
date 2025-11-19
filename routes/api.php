2858702c1c0ffaec96c7b7239a3c41823bc64424
Johan Rincón <johanestebanrincon@gmail.com>
2025-11-19 00:05:06 -0500

    se agrega ruta api de personalizacion

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\PersonalizacionController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('usuarios', UsuarioController::class);
 Route::apiResource('categorias', CategoriaController::class);
 Route::apiResource('productos', ProductoController::class);
+Route::apiResource('personalizaciones', PersonalizacionController::class);
