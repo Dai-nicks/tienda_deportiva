508234710fe7c7f2e662f9ded71880def030b2e3
Dai <dainicks08@yahoo.com>
2025-11-28 00:36:18 -0500

    Rutas para CRUD DetallePedido

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\PersonalizacionController;
 use App\Http\Controllers\CarritoController;
 use App\Http\Controllers\DetalleCarritoController;
+use App\Http\Controllers\DetallePedidoController;
+use App\Http\Controllers\PagoController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,6 +16,8 @@ Route::apiResource('usuarios', UsuarioController::class);
 Route::apiResource('categorias', CategoriaController::class);
 Route::apiResource('productos', ProductoController::class);
 Route::apiResource('personalizaciones', PersonalizacionController::class);
-Route::apiResource('carritos', CarritoController::class); 
+Route::apiResource('carritos', CarritoController::class);
 Route::apiResource('detalles_carrito', DetalleCarritoController::class);
+Route::apiResource('detalles_pedido', DetallePedidoController::class);
+Route::apiResource('pagos', PagoController::class);
 Route::post('/login', [AuthController::class, 'login']);
