1b9d0131d4d375bc5143f9b7f56617b1d6863d57
Dai <dainicks08@yahoo.com>
2025-11-28 00:45:22 -0500

    Corregir rutas: eliminar pagos y duplicado de carritos

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\DetallePedidoController;
 use App\Http\Controllers\PagoController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::apiResource('usuarios', UsuarioController::class);
 Route::apiResource('categorias', CategoriaController::class);
 Route::apiResource('productos', ProductoController::class);
